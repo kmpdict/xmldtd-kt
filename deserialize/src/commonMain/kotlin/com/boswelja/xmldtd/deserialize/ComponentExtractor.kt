306cf6e15e1bb7aea4593aae6c9c166504438181
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-07-27 13:05:49 +1000

    Initial multiline attlist implementation

--- deserialize/src/commonMain/kotlin/com/boswelja/xmldtd/deserialize/ComponentExtractor.kt
+++ deserialize/src/commonMain/kotlin/com/boswelja/xmldtd/deserialize/ComponentExtractor.kt
@@ -2,6 +2,7 @@ package com.boswelja.xmldtd.deserialize
 
 import kotlinx.io.Source
 import kotlinx.io.readLine
+import kotlinx.io.readLineStrict
 
 internal val CommentRegex = Regex("<!--([\\s\\S\\n]*?)-->")
 
@@ -56,6 +57,9 @@ internal data class AttributeDto(
     companion object {
         internal val AttributeListRegex = Regex("<!ATTLIST\\s+([a-zA-Z0-9_-]+)\\s+([a-zA-Z0-9:_-]+)\\s+([a-zA-Z0-9_-]+)\\s+([\"#a-zA-Z0-9_-]+)\\s*>")
 
+        internal val MultilineAttlistStartRegex = Regex("<!ATTLIST\\s+([a-zA-Z0-9_-]+)")
+        internal val MultilineAttlistItemRegex = Regex("([a-zA-Z0-9:_-]+)[^\\S\\r\\n]+([a-zA-Z0-9_-]+)[^\\S\\r\\n]+([\"#a-zA-Z0-9_-]+)[^\\S\\r\\n]*(>)?")
+
         internal fun fromLine(line: String): AttributeDto? {
             return AttributeListRegex.find(line)?.let { matchResult ->
                 val elementName = matchResult.groupValues[1]
@@ -69,6 +73,33 @@ internal data class AttributeDto(
                     value = value,
                     comment = null,
                 )
+            }
+        }
+
+        internal fun fromLines(currentLine: String, source: Source): List<AttributeDto>? {
+            val peekedLine = currentLine
+            return MultilineAttlistStartRegex.find(peekedLine)?.let { matchResult ->
+                val elementName = matchResult.groupValues.last()
+                buildList {
+                    while (!source.exhausted()) {
+                        val line = source.readLineStrict().trim()
+                        val matchResult = MultilineAttlistItemRegex.find(line) ?: continue
+                        val attrName = matchResult.groupValues[1]
+                        val type = matchResult.groupValues[2]
+                        val value = matchResult.groupValues[3]
+                        add(
+                            AttributeDto(
+                                elementName = elementName,
+                                attributeName = attrName,
+                                type = type,
+                                value = value,
+                                comment = null,
+                            )
+                        )
+                        // If the end attlist character is present, stop iterating.
+                        if (matchResult.groupValues.last() == ">") break
+                    }
+                }
             }
         }
     }
